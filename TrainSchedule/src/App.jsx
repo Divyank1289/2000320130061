6b07c622ee037aed800344e9467920a9ba7bc0da
Divyank Trivedi <divyanktrivedi0@gmail.com>
2023-07-22 12:55:58 +0530

    Output Added

--- TrainSchedule/src/App.jsx
+++ TrainSchedule/src/App.jsx
@@ -1,39 +1,48 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import TrainTable from "./TrainT";
+import { Route, Routes } from "react-router-dom";
+import Train from "./train";
 
-const body1 = {
+const data1 = {
+  clientID: "3c108fba-ab78-443a-ac65-587403bd33c0",
+  clientSecret: "OGgxNQPpRiWmNuFO",
   companyName: "Train Central",
+  ownerEmail: "Divyank.20B0131151@abes.ac.in",
   ownerName: "Divyank Trivedi",
   rollNo: "0061",
-  ownerEmail: "Divyank.20B0131151@abes.ac.in",
-  accessCode: "qxrwbC"
-}
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState();
+  // console.log(token);
 
   const fetchData = async () => {
-    const fetchedData = await fetch("http://20.244.56.144/train/register", {
+    const fetchedData = await fetch("http://20.244.56.144/train/auth", {
       method: "POST",
       headers: {
-        'Content-Type': "application/json"
+        "Content-Type": "application/json",
       },
-
-      body: JSON.stringify(body1)
+      body: JSON.stringify(data1),
     });
     const data = await fetchedData.json();
-    console.log(data);
-  }
+    setToken(data.access_token);
+  };
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-
+      <Routes>
+        {token && <Route path="/" element={<TrainTable token={token} />} />}
+        {token && (
+          <Route path="/train/:trainNumber" element={<Train token={token} />} />
+        )}
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
